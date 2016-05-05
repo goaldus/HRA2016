b9c991c734caf86ae323a554717a81c4a0816827
Ondřej Molnár <chsicht@atlas.cz>
2016-05-05 18:28:24 +0200

    hustoles gameboard update

--- src/gameboard.cpp
+++ src/gameboard.cpp
@@ -16,6 +16,7 @@
 * @author Vilem Jelen, Ondrej Molnar
 */
 
+#include <vector>
 #include <iostream>
 
 #include "square.h"
@@ -27,7 +28,6 @@ using namespace std;
 GameBoard::GameBoard() {
 	BlackOnTurn = true;
 	enemyAI = false;
-	AItype = 0;
 	size = 8;
 	rght = 1;
 	lft = -1;
@@ -38,6 +38,9 @@ GameBoard::GameBoard() {
 	lfbot = 7;
 	rghbot = 9;
 	mid = 4; // to get position from which I can initialize stones
+	whites.reserve(7 * size);
+	blacks.reserve(7 * size);
+	available.reserve(10);
 }
 
 //Overloaded Gameboard constructor with input size(val)
@@ -46,6 +49,7 @@ GameBoard::GameBoard(short int size, short int AItype) {
 	enemyAI = (AItype != 0) ? true : false;
 	this->AItype = AItype;
 	this->size = size;
+	/*Directions*/
 	rght = 1;
 	lft = -1;
 	rghtop = -size + 1;
@@ -54,7 +58,11 @@ GameBoard::GameBoard(short int size, short int AItype) {
 	bot = size;
 	lfbot = size - 1;
 	rghbot = size + 1;
-	mid = size - 1 * size / 2; // to get position from which I can initialize stones
+	mid = size - 1 * size / 2;
+	/*Vector reservation*/
+	whites.reserve(7 * size);
+	blacks.reserve(7 * size);
+	available.reserve(10);
 }
 
 /*
@@ -76,6 +84,114 @@ void GameBoard::Init(Square ** sq)
 	sq[offset]->owner = 2;
 	sq[offset + size]->owner = 1;
 	sq[offset + size - 1]->owner = 2;
+
+	whites.push_back(offset - 1);
+	whites.push_back(offset + size);
+	blacks.push_back(offset);
+	blacks.push_back(offset + size - 1);
+
+}
+
+/*
+@brief Function places stone to gameboard's square and adds index to vector so I can work only with selected color
+*/
+void GameBoard::placeStone(Square *sq, int num)
+{
+	if (BlackOnTurn)
+	{
+		sq->owner = 2;
+		blacks.push_back(num);
+	}
+	else
+	{ 
+		sq->owner = 1;
+		whites.push_back(num);
+	}
+}
+
+/*
+@brief Changes turn to next player
+*/
+void GameBoard::nextTurn()
+{
+	BlackOnTurn = !BlackOnTurn;
+}
+
+/*
+@brief Checks square and if he is empty than show user that he can plant there
+*/
+void GameBoard::checkPlace(short int pos, short int dir, Square **sq, short int enemy, short int me)
+{
+	short int index = pos + dir;
+
+	if (index >= size*size || sq[index]->owner == me) return;
+	else
+	{
+		if (sq[index]->owner == 0)
+		{
+			sq[index]->owner = 3;
+			available.push_back(index);
+		}
+		else
+			checkPlace(index, dir, sq, enemy, me);
+	}
+}
+
+/*
+@brief This function checks possibilities to end and then calls function checkPlace()
+*/
+void GameBoard::pes(short int pos, short int dir, Square **sq)
+{
+	short int index = pos + dir;
+	short int enemy;
+	short int me;
+
+	if (BlackOnTurn)
+	{
+		enemy = 1;
+		me = 2;
+	}
+	else
+	{
+		enemy = 2;
+		me = 1;
+	}
+	if (index >= size*size || sq[index]->owner == me) return; // OUT OF FIELD? OR DO I OWN THIS DISC? THEN GO TO HELL
+	else
+		if (sq[index]->owner == 0 || sq[index]->owner == 3) return; // IS FIRST SQUARE EMPTY? THEN END MAN!
+		else //NOT EMPTY AND NOT MINE SO GO FOR TESTING
+		checkPlace(index, dir, sq, enemy, me);
+}
+
+void GameBoard::neco(short int pos, Square**squares)
+{
+	pes(pos, top, squares);
+	pes(pos, rghtop, squares);
+	pes(pos, rght, squares);
+	pes(pos, rghbot, squares);
+	pes(pos, bot, squares);
+	pes(pos, lfbot, squares);
+	pes(pos, lft, squares);
+	pes(pos, lftop, squares);
+}
+
+void GameBoard::setAvailables(Square **squares)
+{
+	for (int i = 0; i < available.size(); i++)
+	{
+		if (squares[available[i]]->owner == 3)
+		squares[available[i]]->owner = 0;
+	}
+		
+	available.clear();
+	vector<short int> *vecptr;
+	if (BlackOnTurn)
+		vecptr = &blacks;
+	else
+		vecptr = &whites;
+
+	for (int i = 0; i < (*vecptr).size(); i++)
+		neco((*vecptr)[i], squares);
 }
 
 /*** End of file gameboard.cpp ***/
--- src/gameboard.h
+++ src/gameboard.h
@@ -18,6 +18,8 @@
 
 #pragma once
 
+#include <vector>
+
 class GameBoard {
 	short int rght;
 	short int lft;
@@ -29,6 +31,9 @@ class GameBoard {
 	short int rghbot;
 	short int mid;
 	short int AItype;
+	std::vector<short int> whites;
+	std::vector<short int> blacks;
+	std::vector<short int> available;
 
 public:
 	short int size;
@@ -38,6 +43,12 @@ public:
 	GameBoard(short int, short int);
 	short int getAIType();
 	void Init(Square **);
+	void placeStone(Square *, int);
+	void nextTurn();
+	void checkPlace(short int, short int, Square **, short int, short int);
+	void neco(short int, Square**);
+	void pes(short int, short int, Square **);
+	void setAvailables(Square **);
 };
 
 /*** End of file gameboard.h ***/
